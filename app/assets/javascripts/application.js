b185677b9de468d769004d47171b6d8ee01a0798
Jesús Di Bari <iijbari@gmail.com>
2017-06-18 06:43:00 +0000

    Add style and animation to buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
+//= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
 //  require turbolinks
 //= require_tree .
